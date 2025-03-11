efb60be1c5c11442103b2a98ee624b5a0ea703e6
Sebastian Rodriguez <sebastian.rodriguez.1300@gmail.com>
2025-03-11 00:12:51 -0400

    Updating to have examples of malloc and custom structs

--- ch_0/second_project/main.cpp
+++ ch_0/second_project/main.cpp
@@ -3,7 +3,13 @@
 // C++ program to show Arrow operator
 // used in structure
 #include <iostream>
+#include "my_class.h"
+#include "math_constants.h"
+
+//using namespace std;
+using namespace N;
 using namespace std;
+using namespace math_consts;
 
 // Creating the structure
 struct student {
@@ -14,10 +20,26 @@ struct student {
 
 // Creating the structure object
 struct student* emp = NULL;
+struct student* emp_2 = NULL;
 
 // Driver code
 int main()
 {
+    my_class mc;
+    mc.do_something();
+
+    your_class yc;
+    yc.do_nothing();
+
+    cout << "PI: " << math_consts::pi << endl;
+    cout << "PHI: " << math_consts::phi << endl;
+    cout << "Gravity: " << math_consts::gravity << endl;
+    cout << pi + gravity << endl;
+
+    cout << "I'm changing gravity" << endl;
+    math_consts::gravity = 8;
+    cout << "Gravity: " << math_consts::gravity << endl;
+
     /*
     * Practicing comments
     *
@@ -26,12 +48,16 @@ int main()
     emp = (struct student*)
         malloc(sizeof(struct student));
 
+    emp_2 = (struct student*)malloc(sizeof(struct student));
+    emp_2->percentage = 0.4;
+
     // Assigning value to age variable
     // of emp using arrow operator
     emp->age = 18;
 
     // Printing the assigned value to the variable
-    cout <<" "<< emp->age;
-
+    cout <<" "<< emp->age << endl;
+    cout <<" "<< emp_2->percentage << endl;
+    free(emp);
     return 0;
 }
